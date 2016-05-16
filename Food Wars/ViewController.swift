24b3fe211e85094bc8c0c24ac6ff9767499b9245
Cecilia Giorcelli <ceciliagiorcelli@Cecilias-MacBook-Pro-3.local>
2016-05-16 13:51:46 -0400

    Food Wars iOS final

--- Food Wars/ViewController.swift
+++ Food Wars/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TopBtn: UIButton!
+    @IBOutlet weak var BtmBtn: UIButton!
+    
+    @IBOutlet weak var GrnLabel: UILabel!
+    
+    @IBOutlet weak var endScene: UILabel!
+    
+    @IBOutlet weak var TopEndLbl: UILabel!
+    @IBOutlet weak var BtmEndLbl: UILabel!
+    @IBOutlet weak var RedLabel: UILabel!
+    @IBOutlet weak var NewGameBtn: UIButton!
+    
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        score = 0
+        GrnLabel.text = "\(score)"
+        RedLabel.text = "\(score)"
+        
+        GrnLabel.transform = CGAffineTransformMakeRotation(3.14)//rotate upside down for other player
+        
+        //hides end label and 'new game' button until someone loses/wins
+            endScene.hidden = true
+            TopEndLbl.hidden = true
+            BtmEndLbl.hidden = true
+            NewGameBtn.hidden = true
+        
+        TopEndLbl.transform = CGAffineTransformMakeRotation(3.14)//rotate upside down for other player
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +47,57 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func GrnBtnAction(sender: AnyObject) {
+        score+=1
+        GrnLabel.text = "\(score)"
+        RedLabel.text = "\(score)"
+        testScore()
+    }
 
+    @IBAction func RedBtnAction(sender: AnyObject) {
+    score-=1
+    GrnLabel.text = "\(score)"
+    RedLabel.text = "\(score)"
+        
+    testScore()
+    }
+    
+    func testScore(){
+        if score >= 10 {
+            //show end screen if 10 is reached
+            endScene.hidden = false
+            TopEndLbl.hidden = false
+            BtmEndLbl.hidden = false
+            NewGameBtn.hidden = false
+            
+            //display loser and winner text
+            TopEndLbl.text = "🐮 Wins"
+            BtmEndLbl.text = "🐯 Loses"
+            
+        }else if score <= -10{
+            //show end screen if -10 is reached
+           endScene.hidden = false
+            TopEndLbl.hidden = false
+            BtmEndLbl.hidden = false
+            NewGameBtn.hidden = false
+            
+            //display loser and winner text
+            TopEndLbl.text = "🐮 Loses"
+            BtmEndLbl.text = "🐯 Wins"
+            
+        }
+    }
+    @IBAction func NewGameBtn(sender: AnyObject) {
+        //when end screen is tapped, hide end screen and re-start game at 0
+        endScene.hidden = true
+        TopEndLbl.hidden = true
+        BtmEndLbl.hidden = true
+        NewGameBtn.hidden = true
+        
+        //start new game at 0
+        score = 0
+        GrnLabel.text = "\(score)"
+        RedLabel.text = "\(score)"
+    }
 }
 
