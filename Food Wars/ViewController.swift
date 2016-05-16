8aab79f1b30ce877a8097575dff606eabc1032f3
Cecilia Giorcelli <ceciliagiorcelli@cecilias-macbook-pro-3.local>
2016-05-16 12:54:43 -0400

    Initial Commit

---
+++ Food Wars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Wars
+//
+//  Created by Cecilia Giorcelli on 5/16/16.
+//  Copyright © 2016 Cecilia Giorcelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
